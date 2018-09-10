25f10fd3a9368fd4cd6ee1ff3e6238ee36513ae0
granteric498 <granteric498@yahoo.com>
2018-09-10 14:12:29 -0500

    friends matching

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,7 @@
-const friends = [
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+const friends = [{
+        "name": "Robert Downey Jr.",
+        "photo": "https://pbs.twimg.com/profile_images/3583783046/71ec1ad2986c6c282949689e8a578f83_400x400.jpeg",
+        "scores": [
             5,
             1,
             4,
@@ -13,8 +12,88 @@ const friends = [
             5,
             4,
             1
-          ]
-      }      
+        ]
+    },
+    {
+        "name": "Beyonce",
+        "photo": "https://consequenceofsound.files.wordpress.com/2015/12/beyonce-naughty-boy-runnin-lose-it-all.jpg?quality=80&w=400&h=400&crop=1",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Tom Brady",
+        "photo": "http://grid.milkeninstitute.org/events/speakers/W29948.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Michael Jordan",
+        "photo": "https://pbs.twimg.com/profile_images/1004558945577512960/fKHZ8z6r_400x400.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Scarlett Johansson",
+        "photo": "http://1.bp.blogspot.com/_aCYsfpGX07M/SeS3MFbujwI/AAAAAAAABBg/G1B8EjqMwyA/s400/scarlett_johansson.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Donald Glover",
+        "photo": "https://mk0bookingagent97efu.kinstacdn.com/wp-content/uploads/2013/10/Childish-Gambino-Contact-Information.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
 ];
 
 module.exports = friends;
